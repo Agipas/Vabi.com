6a9b5049c4d9104b9245e01467e649bebef278e2
i1 <ivansapiga1999@gmail.com>
2016-10-31 19:20:01 +0200

    Update vigenere.c

--- vigenere.c
+++ vigenere.c
@@ -4,21 +4,21 @@
 #include <ctype.h>
 /*enter key and text
 */
-int main(int argc, string argv[]) {
+int main(int argc, string argv[]) {//check input
 	if (argc != 2) {
-	printf("Try agayn,pls");
+		printf("Try agayn,pls");
 	return 1;
 	}
-	string key = argv[1];
+		string key = argv[1];
 	unsigned long keyCount = strlen(key);
 	for (int a = 0; a < keyCount; a++) {
 	if (!isalpha(key[a])) {
-	printf("Try agayn,pls");
+		printf("Try agayn,pls");
 	return 1;}
 	}
-	string text = GetString();
+		string text = GetString();
 	if (text == NULL) {
-	printf("Try agayn,pls");
+		printf("Try agayn,pls");
 	return 1;
 	}
 	//coding text
@@ -26,13 +26,13 @@ int main(int argc, string argv[]) {
 	if (isalpha(text[i])) {
 	// Capital letter
 	if (isupper(text[i]))
-	printf("%c", ((((text[i] - 'A') + ((toupper(key[n++%keyCount])) - 'A') % 26) % 26) + 'A'));
+			printf("%c", ((((text[i] - 'A') + ((toupper(key[n++%keyCount])) - 'A') % 26) % 26) + 'A'));
 	//Small letter
 	if (islower(text[i]))
-	printf("%c", ((((text[i] - 'a') + ((tolower(key[n++%keyCount])) - 'a') % 26) % 26) + 'a'));}
+		printf("%c", ((((text[i] - 'a') + ((tolower(key[n++%keyCount])) - 'a') % 26) % 26) + 'a'));}
 	else
-	printf("%c", text[i]);
+		printf("%c", text[i]);
 	}
-	printf("\n");
+		printf("\n");
 	return 0;
 }
